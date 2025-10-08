eee7762b322d59c8aa6bee7a0431dd6adbb12264
MBCJ <163526992+MarceloBarretoCelestinoJunior@users.noreply.github.com>
2025-10-08 13:52:31 +0000

    Adição do mapa interativo e API com .env

--- src/server.js
+++ src/server.js
@@ -1,13 +1,24 @@
+require('dotenv').config();
 
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static('public'))
+
+app.get('/api/key', (req, res) => {
+  res.json({ apiKey: process.env.GOOGLE_MAPS_API_KEY });
+});
+
+// Rota principal para o index
 app.get('/', (req, res) => {
-  res.send('Hello, world!');
+  res.sendFile(path.join(__dirname, '../public/html/index.html'));
 });
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Acesse localhost:${PORT}`);
+  
 });
   
